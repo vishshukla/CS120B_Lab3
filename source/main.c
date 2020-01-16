016fb67f3f505218e69fd032a6dc2e044bed5550
Nicole Nguyen <nnguy099@wch136-17.cs.ucr.edu>
2020-01-15 20:46:36 -0800

    merge_msg.swp error; pushing attempt at part2

--- source/main.c
+++ source/main.c
@@ -15,30 +15,30 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0x00; PORTB = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	unsigned char tmpA = 0x00;
-	unsigned char tmpB = 0x00;
-	unsigned char count = 0x00;
+	unsigned char tmpC = 0x00;
 
     /* Insert your solution below */
     while (1) {
 	// Read input
 	tmpA = PINA & 0xFF;
-	tmpB = PINB & 0xFF;
-	count = 0x00;
+	tmpC = 0xFF;
 	
-	while (tmpA) {
-		count += tmpA & 0x01;
-		tmpA >>= 1;
-	}
-
-	while (tmpB) {
-		count += tmpB & 0x01;
-		tmpB >>= 1;
-	}
+	if ((tmpA & 0x01) | 0x02) {
+		tmpC = tmpC & 0x60;
+	} else if ((tmpA & 0x03) | 0x04) {
+		tmpC = tmpC & 0x70;
+	} else if ((tmpA & 0x05) | 0x06) {
+		tmpC = tmpC & 0x38;
+	} else if ((tmpA & 0x07) | 0x08 | 0x09) {
+		tmpC = tmpC & 0x3C;
+	} else if ((tmpA & 0x0A) | 0x0B | 0x0C) {
+		tmpC = tmpC & 0x3E;
+	} else { // if ((tmpA & 0x0D) | 0x0E | 0x0F) {
+		tmpC = tmpC & 0x3F;
 	
-	PORTC = count;	
+	PORTC = tmpC;	
     }
 return 0;
 }
